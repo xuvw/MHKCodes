1e489b7917cc54f3fd3ea17ea2c8dedd749c1d92
ke he <smileshitou@hotmail.com>
2015-10-28 23:41:21 +0800

    setup

--- MHKCodes/AppDelegate.m
+++ MHKCodes/AppDelegate.m
@@ -7,6 +7,7 @@
 //
 
 #import "AppDelegate.h"
+#import "ViewController.h"
 
 @interface AppDelegate ()
 
@@ -16,7 +17,15 @@
 
 
 - (BOOL)application:(UIApplication *)application didFinishLaunchingWithOptions:(NSDictionary *)launchOptions {
-    // Override point for customization after application launch.
+    self.window = [[UIWindow alloc] initWithFrame:[[UIScreen mainScreen] bounds]];
+    self.window.backgroundColor = [UIColor blackColor];
+    
+    ViewController *vc = [[ViewController alloc] init];
+    vc.view.backgroundColor = [UIColor whiteColor];
+    self.window.rootViewController = vc;
+    
+    [self.window makeKeyAndVisible];
+    
     return YES;
 }
 
